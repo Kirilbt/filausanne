3831a333dbcb79f8f967166f6d838e86a64c0540
Patrick Almeida <patrick.c.almeida@gmail.com>
2022-03-30 13:47:57 +0200

    sleep_index-open/closed without opens/closes at

--- app/controllers/sleeps_controller.rb
+++ app/controllers/sleeps_controller.rb
@@ -8,20 +8,6 @@ class SleepsController < ApplicationController
   def show
   end
 
-  def open?(open, close)
-    Time.now.hour > open.hour && Time.now.hour < close.hour ? "Open" : "Closed"
-  end
-
-  def open_close_time(open, close)
-    if open?(open, close) == "Open"
-      "Closes at #{close.strftime('%H:%M')}"
-    else
-      "Opens at #{open.strftime('%H:%M')}"
-    end
-  end
-
-  helper_method :open?, :open_close_time
-
   private
 
   def set_sleep
