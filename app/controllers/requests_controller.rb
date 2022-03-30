61d4ee09a9a3a4c511f52d9d0525feca8958216a
Patrick Almeida <patrick.c.almeida@gmail.com>
2022-03-30 17:17:07 +0200

    request ok

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,26 @@
+class RequestsController < ApplicationController
+
+  def new
+    @sleep = Sleep.find(params[:sleep_id])
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @sleep = Sleep.find(params[:sleep_id])
+    @request.service = @sleep
+    @request.user = current_user
+    @request.status_translations = "Pending"
+    if @request.save
+      redirect_to sleeps_path
+    else
+      render 'sleeps/show'
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:start_date, :end_date, :sleep_id)
+  end
+end
