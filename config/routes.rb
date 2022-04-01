c762a1c862efe52a723c9d145f522c1f5740f851
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-04-01 12:38:10 +0200

    language button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   scope "(:locale)", locale: /en|fr/ do
     root to: 'pages#home'
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :eats, only: [:index, :show]
     resources :sleeps, only: [:index, :show] do
       resources :requests, only: [:new, :create]
