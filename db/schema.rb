fc41ed1d7e6e8ba47f21b86ecb383aba9fc729ff
Kiril Tucker <kiril.tucker@gmail.com>
2022-04-04 17:28:47 +0200

    Urgent page flow created (but missing design polish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_01_083841) do
+ActiveRecord::Schema.define(version: 2022_04_04_122334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2022_04_01_083841) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.boolean "urgent", default: false
   end
 
   create_table "users", force: :cascade do |t|
