76193a8a1aff9f2db7f4d8ade91c84d46269f147
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-04-05 17:25:26 +0200

    map on index working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,31 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @services = Service.all
+
+    @markers = @services.geocoded.map do |service|
+      if service.type == "Care"
+        {
+          lat: service.latitude,
+          lng: service.longitude,
+          info_window: render_to_string(partial: "shared/info_window", locals: { service: service }),
+          image_url: helpers.asset_url("care.svg")
+        }
+      elsif service.type == "Eat"
+        {
+          lat: service.latitude,
+          lng: service.longitude,
+          info_window: render_to_string(partial: "shared/info_window", locals: { service: service }),
+          image_url: helpers.asset_url("eat.svg")
+        }
+      else
+        {
+          lat: service.latitude,
+          lng: service.longitude,
+          info_window: render_to_string(partial: "shared/info_window", locals: { service: service }),
+          image_url: helpers.asset_url("sleep.svg")
+        }
+      end
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@ Service.create(
   phone_number: "0213157269",
   website: "www.lausanne.ch",
   price: 0,
-  capacity: "38"
+  capacity: "38",
+  urgent: true
 )
 
 OpeningHour.create(day_translations: { fr: "Lundi", en: "Monday" }, opens: "21", closes: "23:59:59", service_id: Service.find_by(name: "L'Étape").id)
@@ -30,7 +31,8 @@ Service.create(
   phone_number: "0213117912",
   website: "www.lausanne.ch",
   price: 0,
-  capacity: 31
+  capacity: 31,
+  urgent: true
 )
 
 OpeningHour.create(day_translations: { fr: "Lundi", en: "Monday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "La Marmotte").id)
@@ -45,11 +47,12 @@ Service.create(
   type: "Sleep",
   name: "Sleep-in",
   description_translations: { fr: "Réservation obligatoire au Bureau des Réservations. Enfants accompagnés d'un adulte admis." },
-  address: "Rue du Vallon 17, 1005 Lausanne",
+  address: "Chemin de l'Usine à Gaz 10, 1020 Renens",
   phone_number: "0213117912",
   website: "www.sleepin-lausanne.ch",
   price: 0,
-  capacity: 26
+  capacity: 26,
+  urgent: true
 )
 
 OpeningHour.create(day_translations: { fr: "Lundi", en: "Monday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Sleep-in").id)
@@ -61,10 +64,49 @@ OpeningHour.create(day_translations: { fr: "Samedi", en: "Saturday" }, opens: "0
 OpeningHour.create(day_translations: { fr: "Dimanche", en: "Sunday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Sleep-in").id)
 
 Service.create(
+  type: "Sleep",
+  name: "Le Répit",
+  description_translations: { fr: "Aucune condition particulière." },
+  address: "Avenue Bergières 10, 1004 Lausanne",
+  phone_number: "021 661 10 15",
+  website: "www.meresofia.ch/structures/le-repit",
+  price: 0,
+  capacity: nil,
+  urgent: true
+)
+
+OpeningHour.create(day_translations: { fr: "Lundi", en: "Monday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Le Répit").id)
+OpeningHour.create(day_translations: { fr: "Mardi", en: "Tuesday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Le Répit").id)
+OpeningHour.create(day_translations: { fr: "Mercredi", en: "Wednesday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Le Répit").id)
+OpeningHour.create(day_translations: { fr: "Jeudi", en: "Thursday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Le Répit").id)
+OpeningHour.create(day_translations: { fr: "Vendredi", en: "Friday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Le Répit").id)
+OpeningHour.create(day_translations: { fr: "Samedi", en: "Saturday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Le Répit").id)
+OpeningHour.create(day_translations: { fr: "Dimanche", en: "Sunday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Le Répit").id)
+
+Service.create(
+  type: "Sleep",
+  name: "Borde 47",
+  description_translations: { fr: "Aucune condition particulière." },
+  address: "Rue de la Borde 47, 1018 Lausanne",
+  phone_number: "079 669 98 97",
+  website: "https://www.lausanne.ch/prestations/service-social/hebergement-nocturne-urgence.html#borde-47-0",
+  price: 0,
+  capacity: nil
+)
+
+OpeningHour.create(day_translations: { fr: "Lundi", en: "Monday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Borde 47").id)
+OpeningHour.create(day_translations: { fr: "Mardi", en: "Tuesday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Borde 47").id)
+OpeningHour.create(day_translations: { fr: "Mercredi", en: "Wednesday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Borde 47").id)
+OpeningHour.create(day_translations: { fr: "Jeudi", en: "Thursday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Borde 47").id)
+OpeningHour.create(day_translations: { fr: "Vendredi", en: "Friday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Borde 47").id)
+OpeningHour.create(day_translations: { fr: "Samedi", en: "Saturday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Borde 47").id)
+OpeningHour.create(day_translations: { fr: "Dimanche", en: "Sunday" }, opens: "09", closes: "18", service_id: Service.find_by(name: "Borde 47").id)
+
+Service.create(
   type: "Eat",
   name: "La Soupe Populaire",
   description_translations: { fr: "Aucune conditon partculière." },
-  address: "Rue du Vallon 17, 1005 Lausanne",
+  address: "Rue Saint-Martin 16 1005 Lausanne",
   phone_number: "0786264322",
   website: "http://www.meresofia.ch/structures/la-soupe-populaire/",
   price: 0
