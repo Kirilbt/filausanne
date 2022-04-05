8a60673393eaf2785badb4ee037d63728064d24e
Kiril Tucker <kiril.tucker@gmail.com>
2022-04-05 11:03:14 +0200

    Services listed under the urgent page now redirect to the correct service show

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   scope "(:locale)", locale: /en|fr|pt/ do
     root to: 'pages#home'
-    # resources :services
+    resources :services, only: [:show]
     resources :eats, only: [:index, :show, :edit, :update]
     resources :sleeps, only: [:index, :show, :edit, :update] do
       resources :requests, only: [:new, :create]
     end
     resources :cares, only: [:index, :show, :edit, :update]
     resources :requests, only: [:index, :edit, :update]
-    get 'urgent', to: 'pages#urgent'
+    get 'urgent', to: 'services#urgent'
   end
-  # get 'urgent', action: :show, controller: 'services'
 end
